036ca864c5f09d1a00992369ad8d6906850f81e9
greenlemonT <ms950219@naver.com>
2024-11-06 13:20:51 +0900

    spring 프로젝트 생성

---
+++ Backend/myPharm/src/main/java/myPharm/myPharm/MyPharmApplication.java
@@ -0,0 +1,13 @@
+package myPharm.myPharm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPharmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPharmApplication.class, args);
+	}
+
+}
---
+++ Backend/myPharm/src/test/java/myPharm/myPharm/MyPharmApplicationTests.java
@@ -0,0 +1,13 @@
+package myPharm.myPharm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPharmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
